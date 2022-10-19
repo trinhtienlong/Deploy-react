27083fe5b3b0623f35c50033d077bd36e91583b5
trinhtienlong <trinhtienlong1998@gmail.com>
2022-10-19 18:06:18 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <div className="row">
               <div className="col-lg-7">
                 <div className="header-text">
-                  <h6>Welcome To Cyborg</h6>
+                  <h6>Welcome To Cyborg test</h6>
                   <h4><em>Browse</em> Our Popular Games Here</h4>
                   <div className="main-button">
                     <Link to="browse">Browse Now</Link>
